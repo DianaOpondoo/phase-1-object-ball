8027e0e03e36725691553a118f9fd1c96bd5ea4c
Diana Awino Opondo <dianaawino1@gmail.com>
2022-06-12 18:14:32 +0300

    commit tree 81971a69bfc8fc6244dcd22d7a89a38ea3aa7fb1

---
+++ m.js
@@ -0,0 +1,12 @@
+let artists = [
+    "Smash Mouth",
+    "Janelle Monae",
+    "SZA",
+    "Thunder Cat",
+    "Frank Ocean",
+    "Solange",
+  ];
+  
+  artists._________(function (artist) {
+    return artist.startsWith("S");
+  });
